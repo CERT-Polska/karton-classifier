271566c36051a914e75a5efac3100bc37953109e
msm-code <msm@tailcall.net>
2021-05-19 12:26:12 +0200

    Add a description (#24)

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ setup(
     description="File type classifier for Karton framework",
     namespace_packages=["karton"],
     packages=["karton.classifier"],
+    url="https://github.com/CERT-Polska/karton-classifier/",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
         'console_scripts': [
