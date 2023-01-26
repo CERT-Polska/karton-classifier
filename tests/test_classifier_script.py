e2c3abce120dc1dd5f3cf4b2a27bae079fed5368
Jasper Lievisse Adriaanse <j@jasper.la>
2023-01-26 14:39:01 +0100

    Add tests for scripting languages added in #54

--- tests/test_classifier_script.py
+++ tests/test_classifier_script.py
@@ -102,3 +102,141 @@ class TestClassifier(KartonTestCase):
             },
         )
         self.assertTasksEqual(res, [expected])
+
+    def test_process_script_php(self):
+        resource = mock_resource("script.php")
+        magic = self.magic_from_content(resource.content, mime=False)
+        res = self.run_task(mock_task(resource))
+
+        expected = Task(
+            headers={
+                "type": "sample",
+                "stage": "recognized",
+                "origin": "karton.classifier",
+                "quality": "high",
+                "kind": "script",
+                "mime": "text/x-php",
+                "extension": "php",
+            },
+            payload={
+                "sample": resource,
+                "tags": ["script:php"],
+                "magic": magic,
+            },
+        )
+        self.assertTasksEqual(res, [expected])
+
+    def test_process_script_bash(self):
+        resource = mock_resource("script.bash")
+        magic = self.magic_from_content(resource.content, mime=False)
+        res = self.run_task(mock_task(resource))
+
+        expected = Task(
+            headers={
+                "type": "sample",
+                "stage": "recognized",
+                "origin": "karton.classifier",
+                "quality": "high",
+                "kind": "script",
+                "mime": "text/x-shellscript",
+                "extension": "sh",
+            },
+            payload={
+                "sample": resource,
+                "tags": ["script:sh"],
+                "magic": magic,
+            },
+        )
+        self.assertTasksEqual(res, [expected])
+
+    def test_process_script_perl(self):
+        resource = mock_resource("script.pl")
+        magic = self.magic_from_content(resource.content, mime=False)
+        res = self.run_task(mock_task(resource))
+
+        expected = Task(
+            headers={
+                "type": "sample",
+                "stage": "recognized",
+                "origin": "karton.classifier",
+                "quality": "high",
+                "kind": "script",
+                "mime": "text/x-perl",
+                "extension": "pl",
+            },
+            payload={
+                "sample": resource,
+                "tags": ["script:pl"],
+                "magic": magic,
+            },
+        )
+        self.assertTasksEqual(res, [expected])
+
+    def test_process_script_python(self):
+        resource = mock_resource("script.py")
+        magic = self.magic_from_content(resource.content, mime=False)
+        res = self.run_task(mock_task(resource))
+
+        expected = Task(
+            headers={
+                "type": "sample",
+                "stage": "recognized",
+                "origin": "karton.classifier",
+                "quality": "high",
+                "kind": "script",
+                "mime": "text/x-python",
+                "extension": "py",
+            },
+            payload={
+                "sample": resource,
+                "tags": ["script:py"],
+                "magic": magic,
+            },
+        )
+        self.assertTasksEqual(res, [expected])
+
+    def test_process_script_ruby(self):
+        resource = mock_resource("script.rb")
+        magic = self.magic_from_content(resource.content, mime=False)
+        res = self.run_task(mock_task(resource))
+
+        expected = Task(
+            headers={
+                "type": "sample",
+                "stage": "recognized",
+                "origin": "karton.classifier",
+                "quality": "high",
+                "kind": "script",
+                "mime": "text/x-ruby",
+                "extension": "rb",
+            },
+            payload={
+                "sample": resource,
+                "tags": ["script:rb"],
+                "magic": magic,
+            },
+        )
+        self.assertTasksEqual(res, [expected])
+
+    def test_process_script_script(self):
+        resource = mock_resource("script.scpt")
+        magic = self.magic_from_content(resource.content, mime=False)
+        res = self.run_task(mock_task(resource))
+
+        expected = Task(
+            headers={
+                "type": "sample",
+                "stage": "recognized",
+                "origin": "karton.classifier",
+                "quality": "high",
+                "kind": "script",
+                "mime": "application/octet-stream",
+                "extension": "scpt",
+            },
+            payload={
+                "sample": resource,
+                "tags": ["script:scpt"],
+                "magic": magic,
+            },
+        )
+        self.assertTasksEqual(res, [expected])
