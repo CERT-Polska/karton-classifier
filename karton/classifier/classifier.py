ede016a878c3adad36772cf188244944229fdcab
Gerhard Stöckli <stoeckli@macgs.switch.ch>
2023-07-10 11:30:27 +0200

    FEATURE: detect steam files

--- karton/classifier/classifier.py
+++ karton/classifier/classifier.py
@@ -705,23 +705,19 @@ class Classifier(Karton):
                 }
             )
 
-        #if sample_class['kind'] is None:
-        #    # as libmagic sometimes fails to detect encoding of text files
-        #    chardet_prediction = chardet.detect(partial)['encoding']
-        #    self.log.info(f'chardet classification: {chardet_prediction} magic: {magic}')
-        #    if chardet_prediction is not None:
-        #        sample_class.update(
-        #            {
-        #                "kind": chardet_prediction
-        #            }
-        #        )
-
         # Heuristics for scripts
         try:
             partial_str = partial.decode(chardet.detect(partial)["encoding"]).lower()
         except Exception:
             self.log.warning("Heuristics disabled - unknown encoding")
-            partial_str = None
+            # Detect steam files based on the file name
+            if re.match('ssfn\d{16,19}', file_name):
+                sample_class.update(
+                    {
+                        "kind": "steam",
+                    }
+                )
+            return sample_class
 
         if partial_str:
             vbs_keywords = [
